bd81e570cc69f78a364ebdbbc01be96ae678cb79
avishay <avgdrs23@gmail.com>
2021-12-15 21:23:38 +0200

    first commit

--- gitAdd.sh
+++ gitAdd.sh
@@ -2,5 +2,7 @@
 
 #create git  repo to this project and push it to gitHub website
 repo=$(basename $PWD)
-git init; git add .; git branch -m master;  git commit -m "first commit"; git remote add origin git@github.com:avgdrs23/"${repo}"
+git init; git add .
+#git branch -m master;  
+git commit -m "first commit"; git remote add origin git@github.com:avgdrs23/"${repo}"
 git push -u  origin master
